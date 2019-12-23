f4b448d304c22f7bf9c8b2675707d6924fbaba54
cbdev <cb@cbcdn.com>
2019-12-23 17:56:38 +0100

    Add some comments to the layering example

--- configs/layering.lua
+++ configs/layering.lua
@@ -1,9 +1,15 @@
+-- This global variable has the current base offset for the input channels.
+-- We want to map 16 input channels (from MIDI) to 512 output channels (ArtNet),
+-- so we have 32 possible offsets (32 * 16 = 512)
 current_layer = 0
 
+-- Set the current_layer based on the control input channel
 function control(value)
 	current_layer = math.floor(value * 31.99);
 end
 
+-- Handler functions for the input channels
+-- Calculate the channel offset and just output the value the input channel provides
 function in0(value)
 	output("out"..((current_layer * 16)), value)
 	print("Output on out"..((current_layer * 16)))

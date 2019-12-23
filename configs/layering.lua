535181b25ccbc492f741b9eeb111a02dfd038b41
cbdev <cb@cbcdn.com>
2019-12-23 20:22:14 +0100

    Fix lua order of operations

--- configs/layering.lua
+++ configs/layering.lua
@@ -12,65 +12,65 @@ end
 -- Calculate the channel offset and just output the value the input channel provides
 function in0(value)
 	output("out"..((current_layer * 16)), value)
-	print("Output on out"..((current_layer * 16)))
+	print("Output on out"..(current_layer * 16))
 end
 
 function in1(value)
-	output("out"..((current_layer * 16)) + 1, value)
+	output("out"..((current_layer * 16) + 1), value)
 end
 
 function in2(value)
-	output("out"..((current_layer * 16)) + 2, value)
+	output("out"..((current_layer * 16) + 2), value)
 end
 
 function in3(value)
-	output("out"..((current_layer * 16)) + 3, value)
+	output("out"..((current_layer * 16) + 3), value)
 end
 
 function in4(value)
-	output("out"..((current_layer * 16)) + 4, value)
+	output("out"..((current_layer * 16) + 4), value)
 end
 
 function in5(value)
-	output("out"..((current_layer * 16)) + 5, value)
+	output("out"..((current_layer * 16) + 5), value)
 end
 
 function in6(value)
-	output("out"..((current_layer * 16)) + 6, value)
+	output("out"..((current_layer * 16) + 6), value)
 end
 
 function in7(value)
-	output("out"..((current_layer * 16)) + 7, value)
+	output("out"..((current_layer * 16) + 7), value)
 end
 
 function in8(value)
-	output("out"..((current_layer * 16)) + 8, value)
+	output("out"..((current_layer * 16) + 8), value)
 end
 
 function in9(value)
-	output("out"..((current_layer * 16)) + 9, value)
+	output("out"..((current_layer * 16) + 9), value)
 end
 
 function in10(value)
-	output("out"..((current_layer * 16)) + 10, value)
+	output("out"..((current_layer * 16) + 10), value)
 end
 
 function in11(value)
-	output("out"..((current_layer * 16)) + 11, value)
+	output("out"..((current_layer * 16) + 11), value)
 end
 
 function in12(value)
-	output("out"..((current_layer * 16)) + 12, value)
+	output("out"..((current_layer * 16) + 12), value)
 end
 
 function in13(value)
-	output("out"..((current_layer * 16)) + 13, value)
+	output("out"..((current_layer * 16) + 13), value)
 end
 
 function in14(value)
-	output("out"..((current_layer * 16)) + 14, value)
+	output("out"..((current_layer * 16) + 14), value)
 end
 
 function in15(value)
-	output("out"..((current_layer * 16)) + 15, value)
+	output("out"..((current_layer * 16) + 15), value)
 end

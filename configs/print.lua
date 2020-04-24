ceefbdb7eddfdccd7b1b6cb452f920382cdae4b0
cbdev <cb@cbcdn.com>
2020-04-25 00:33:57 +0200

    Fix spelling

--- configs/print.lua
+++ configs/print.lua
@@ -1,4 +1,4 @@
--- This function prints the name of the channel it handles and it's value
+-- This function prints the name of the channel it handles and its current value
 -- It can be used for a simple debug output with the `default-handler` configuration option
 function printchannel(value)
 	print(input_channel() .. " @ " .. value)

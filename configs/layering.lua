7693ed34ab5e549b8b7fb8ca395bbf15616d6915
Spacelord <Spacelord09@users.noreply.github.com>
2020-04-26 02:28:11 +0200

    Update layering config to use the new default-handler functionality

--- configs/layering.lua
+++ configs/layering.lua
@@ -3,74 +3,14 @@
 -- so we have 32 possible offsets (32 * 16 = 512)
 current_layer = 0
 
--- Set the current_layer based on the control input channel
-function control(value)
-	current_layer = math.floor(value * 31.99);
-end
-
--- Handler functions for the input channels
--- Calculate the channel offset and just output the value the input channel provides
-function in0(value)
-	output("out"..((current_layer * 16)), value)
-	print("Output on out"..(current_layer * 16))
-end
-
-function in1(value)
-	output("out"..((current_layer * 16) + 1), value)
-end
-
-function in2(value)
-	output("out"..((current_layer * 16) + 2), value)
-end
-
-function in3(value)
-	output("out"..((current_layer * 16) + 3), value)
-end
-
-function in4(value)
-	output("out"..((current_layer * 16) + 4), value)
-end
-
-function in5(value)
-	output("out"..((current_layer * 16) + 5), value)
-end
-
-function in6(value)
-	output("out"..((current_layer * 16) + 6), value)
-end
-
-function in7(value)
-	output("out"..((current_layer * 16) + 7), value)
-end
-
-function in8(value)
-	output("out"..((current_layer * 16) + 8), value)
-end
-
-function in9(value)
-	output("out"..((current_layer * 16) + 9), value)
-end
-
-function in10(value)
-	output("out"..((current_layer * 16) + 10), value)
-end
-
-function in11(value)
-	output("out"..((current_layer * 16) + 11), value)
-end
-
-function in12(value)
-	output("out"..((current_layer * 16) + 12), value)
-end
-
-function in13(value)
-	output("out"..((current_layer * 16) + 13), value)
-end
-
-function in14(value)
-	output("out"..((current_layer * 16) + 14), value)
-end
-
-function in15(value)
-	output("out"..((current_layer * 16) + 15), value)
+function handler(value)
+        if(input_channel() == "control") then
+                -- Set the current_layer based on the control input channel
+                current_layer = math.floor(value * 31.99);
+        else
+                -- Handler functions for the input channels
+                -- Calculate the channel offset and just output the value the input channel provides
+                output("out"..((current_layer * 16) + tonumber(input_channel())), value)
+                print("Output on out"..(current_layer * 16))
+        end
 end
